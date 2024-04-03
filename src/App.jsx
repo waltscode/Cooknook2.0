18e6faf992f9ff492a03aa0d84ff9a78a5d28969
waltscode <walterswork91@outlook.com>
2024-04-03 00:16:29 -0400

    added initial vite config, tailwind config, established shadow cn also

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { Button } from './components/ui/button'
+import './App.css'
+
+
+function App() {
+
+
+  return (
+    <>
+ <Button >Click me</Button>
+    </>
+  )
+}
+
+export default App
