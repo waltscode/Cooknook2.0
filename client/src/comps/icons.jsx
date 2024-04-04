fe23a91d9d20bf7cc226f2ba1fe13b680a8a18d2
waltscode <walterswork91@outlook.com>
2024-04-04 15:13:47 -0400

    added company logo banner

--- client/src/comps/icons.jsx
+++ client/src/comps/icons.jsx
@@ -3,3 +3,45 @@ import React from 'react';
 export function ChefHat(props) {
 	return (<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48" {...props}><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={4} d="M13 24.125a8.64 8.64 0 1 1 3.857-16.837A8.625 8.625 0 0 1 23.64 4a8.627 8.627 0 0 1 6.919 3.464A8.64 8.64 0 1 1 35 24.124V40a2 2 0 0 1-2 2H15a2 2 0 0 1-2-2zM13 31h22m-15-6v6m15-3v6m-22-6v6"></path></svg>);
 }
+
+export function Amazon(props){
+	return(
+		<img src="icons/amazon-opaque.png" alt="" />
+	)
+}
+
+export function EatingWell(props){
+	return(
+		<img src="icons/eatingwellopa.png" alt="" />
+	)
+}
+
+export function FoodWine(props){
+	return(
+		<img src="icons/foodandwineopaque.png" alt="" />
+	)
+}
+
+export function FoodNetwork(props){
+	return(
+		<img src="icons/foodnetopa.png" alt="" />
+	)
+}
+
+export function Forbes(props){
+	return(
+		<img src="icons/forbesopa.png" alt="" />
+	)
+}
+
+export function MensHealth(props){
+	return(
+		<img src="icons/menshealthopa.png" alt="" />
+	)
+}
+
+export function NewYorkTimes(props){
+	return(
+		<img src="icons/NYTopa.png" alt="" />
+	)
+}
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,5 @@
-
+import { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from "../components/ui/card";
+import { Amazon, EatingWell, FoodWine, FoodNetwork, Forbes, MensHealth, NewYorkTimes } from "../comps/icons";
 
 export default function Home() {
 
@@ -9,6 +10,18 @@ export default function Home() {
                     <img src="/images/cooknookwok.png" alt="" />
                     <h1 className="hero-text">From Kitchen to Table: Where Every Recipe Tells a Story.</h1>
                 </div>
+            </div>
+            <div className="mt-2">
+                <Card>
+                    <CardDescription> Featured On: </CardDescription>
+                    <CardContent className='companies'>
+                        <FoodNetwork />
+                        <Amazon />
+                        <EatingWell />
+                        <Forbes />
+                        <MensHealth />
+                    </CardContent>
+                </Card>
             </div>
         </div>
     )
