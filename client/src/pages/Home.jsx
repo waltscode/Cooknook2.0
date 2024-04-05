ecafda750952433a38af9407a4c033616fdd5c3d
waltscode <walterswork91@outlook.com>
2024-04-04 20:13:54 -0400

    added the responsive screen divider for the home page and changed the color of the company icon container

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,11 @@
 import { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from "../components/ui/card";
 import { Amazon, EatingWell, FoodWine, FoodNetwork, Forbes, MensHealth, NewYorkTimes } from "../comps/icons";
-
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious, } from "../components/ui/carousel"
+import {
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup,
+} from "../components/ui/resizable"
 export default function Home() {
 
     return (
@@ -11,9 +16,10 @@ export default function Home() {
                     <h1 className="hero-text">From Kitchen to Table: Where Every Recipe Tells a Story.</h1>
                 </div>
             </div>
+
             <div className="mt-2">
-                <Card>
-                    <CardDescription> Featured On: </CardDescription>
+                <Card className='company-bkg'>
+                    <CardTitle> Featured On: </CardTitle>
                     <CardContent className='companies'>
                         <FoodNetwork />
                         <Amazon />
@@ -22,6 +28,24 @@ export default function Home() {
                         <MensHealth />
                     </CardContent>
                 </Card>
+            </div>
+            <div className="w-full">
+                <ResizablePanelGroup
+                    direction="horizontal"
+                    className="min-h-[400px]  rounded-lg border"
+                >
+                    <ResizablePanel defaultSize={25} style={{ backgroundImage: 'url(images/femalechef.jpeg)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                        <div className="flex h-full items-center justify-center p-6">
+                            <span className="font-semibold">Whether You Are A Professional Chef</span>
+                        </div>
+                    </ResizablePanel>
+                    <ResizableHandle withHandle />
+                    <ResizablePanel defaultSize={75} style={{ backgroundImage: 'url(images/dadcooking.jpeg)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                        <div className="flex h-full items-center justify-center p-6">
+                            <span className="font-semibold">Or A Professional At Home</span>
+                        </div>
+                    </ResizablePanel>
+                </ResizablePanelGroup>
             </div>
         </div>
     )
