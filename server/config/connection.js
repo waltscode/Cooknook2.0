f9fc97697f723a058746cfd85324d6b8b60ac50d
waltscode <walterswork91@outlook.com>
2024-05-27 19:08:56 -0400

    added db url and tested functionality successfully- committing before deploying build command"

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,11 @@
 const {connect, connection} = require('mongoose');
+require('dotenv').config();
 
-connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cooknook');
+connect(process.env.MONGODB_URI);
 
 module.exports = connection;
+
+// connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cooknook');
+
+// connect(process.env.MONGODB_URI, {dbName: 'cooknook' })
+// .then(() => console.log('Connected to the database'));
