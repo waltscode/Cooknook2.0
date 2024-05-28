9e398ed0e5b16b8470cdb9aff1cc4f6e937dc520
waltscode <walterswork91@outlook.com>
2024-05-27 23:28:21 -0400

    removed the html get code

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const routes = require('./routes');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 10000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
@@ -15,15 +15,7 @@ app.use(express.json());
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-  });
 }
-
-app.use(express.static(path.join(__dirname, '../client/dist')));
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-});
 
 app.use(routes);
 
