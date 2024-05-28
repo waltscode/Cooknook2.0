4b36a997fa3e8dd72bde69b6b1249c1be1d108db
waltscode <walterswork91@outlook.com>
2024-05-27 23:07:21 -0400

    changed the port in the vite config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
 
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -10,7 +12,7 @@ export default defineConfig({
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://localhost:10000',
         secure: false,
         changeOrigin: true
       }
