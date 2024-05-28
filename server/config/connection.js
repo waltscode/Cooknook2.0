62bbee1c75c1deeee8a8d530edf94f1fb939df1f
waltscode <walterswork91@outlook.com>
2024-05-27 20:36:14 -0400

    added back the local db ip address for running the "start" script

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const {connect, connection} = require('mongoose');
 require('dotenv').config();
 
-connect(process.env.MONGODB_URI);
+connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cooknook');
 
 module.exports = connection;
 
